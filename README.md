844e897d779f831b449566c48fda3390e9e97bf0
Alex Ayso <alex.ayso@yahoo.com>
2025-01-30 21:03:59 +0800

    chore: init

---
+++ README.md
@@ -0,0 +1,3 @@
+# thepause_audio_player_app
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/thepause_audio_player_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.thepause_audio_player_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
