19dc7c722f8e3f82d7df0313988a35bbddbe598d
Alex Ayso <alex.ayso@yahoo.com>
2025-01-30 21:52:43 +0800

    feat: initial clean architecture

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,35 @@
+import 'package:flutter_clean_architecture/flutter_clean_architecture.dart';
+
+import './src/app/pages/home/home_view.dart';
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MainApp());
-}
+void main() => runApp(const MyApp());
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
+  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    FlutterCleanArchitecture.debugModeOn();
+
+    return MaterialApp(
+      title: 'Flutter Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(
+          // This is the theme of your application.
+          //
+          // Try running your application with "flutter run". You'll see the
+          // application has a blue toolbar. Then, without quitting the app, try
+          // changing the primarySwatch below to Colors.green and then invoke
+          // "hot reload" (press "r" in the console where you ran "flutter run",
+          // or simply save your changes to "hot reload" in a Flutter IDE).
+          // Notice that the counter didn't reset back to zero; the application
+          // is not restarted.
+          // primarySwatch: Colors.blue,
+
+          ),
+      home: const HomePage(title: 'Flutter-based audio player app'),
     );
   }
 }
